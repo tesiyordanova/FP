e9ab737049503094b58cf23cbb69c4bd2745eb98
Teoslava Yordanova <92166912+tesiyordanova@users.noreply.github.com>
2022-07-11 22:45:15 +0300

    Delete week2 directory

--- week2/task4-week2.hs
+++
@@ -1,9 +0,0 @@
-main :: IO()
-main = do 
-    print $ countOccurences 121 1 == 2
-
-countOccurences :: Int -> Int -> Int
-countOccurences x digit
-   | x == digit = 1
-   | mod x 10 == digit = 1 + countOccurences (div x 10) digit
-   | otherwise = countOccurences (div x 10) digit
